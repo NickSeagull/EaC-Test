5b0c0972dd933c33164e253d3af873ba840efd59
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-07-03 12:57:19 +0100

    modified the way of printing in colours because there was a compilation warning of missing parameters

--- src/EaCTest.c
+++ src/EaCTest.c
@@ -1,79 +1,77 @@
 #include <string.h>
 #include <stdio.h>
 
-#define FG_RED 31
-#define FG_GREEN 32
+#define NORMAL_COLOR "\x1B[0m"
+#define FG_RED "\x1B[31m"
+#define FG_GREEN "\x1B[32m"
+#define INIT_COLOR "0x1B[0m"
 #define BG_BLACK 40
-#define INIT_COLOR 0x1B
 
-static int set_green_text(){
-    printf("%c[%d;%d;%dm", INIT_COLOR, FG_GREEN, BG_BLACK);
+void printInGreen(const char* message){
+    printf(FG_GREEN "%s\n", message);
+    printf(NORMAL_COLOR "");
+}
+
+void printInRed(const char* message){
+    printf(FG_RED"%s\n", message);
+    printf(NORMAL_COLOR "");
+}
+
+int pass(char* desc){
+    char message[128] = "\0";
+    strcat(message, desc);
+    strcat(message, " [PASSED]\n");
+    printInGreen(message);
     return 0;
 }
 
-static int set_red_text(){
-    printf("%c[%d;%d;%dm", INIT_COLOR, FG_RED, BG_BLACK);
+int fail(char* desc){
+    char message[128]  = "\0";
+    strcat(message, desc);
+    strcat(message, " [FAILED]");
+    printInRed(message);
     return 0;
 }
 
-static int set_default_text(){
-    printf("%c[%dm\n", INIT_COLOR, 0);
-    return 0;
-}
-
-int pass(char *desc){
-    set_green_text();
-    printf("%s [PASSED]\n", desc);
-    set_default_text();
-    return 0;
-}
-
-int fail(char *desc){
-    set_red_text();
-    printf("%s [FAILED]\n", desc);
-    set_default_text();
-    return 0;
-}
-
-int assertIntEquals(char *msg, int a, int b){
+int assertIntEquals(char* msg, int a, int b){
     if(a == b){
         pass(msg);
     }
     else{
         fail(msg);
-        printf("%d is not %d\n", a, b);
+        printf("  %d is not %d\n\n", a, b);
     }
     return 0;
 }
 
-int assertStringEquals(char *msg, char *a, char *b){
+int assertStringEquals(char* msg, char* a, char* b){
     if(strcmp(a, b) == 0){
         pass(msg);
     }
     else{
         fail(msg);
-        printf("%s is not %s\n",a,b);
+        printf("  %s is not %s\n\n", a, b);
     }
 }
 
-int assertTrue(char *msg, int condition){
+int assertTrue(char* msg, int condition){
     if(condition){
         pass(msg);
     }
     else{
         fail(msg);
-        printf("result was false\n");
+        printf("  result was false\n\n");
     }
     return 0;
 }
 
-int assertFalse(char *msg, int condition){
+int assertFalse(char* msg, int condition){
     if(condition != 0){
         pass(msg);
     }
     else{
         fail(msg);
-        printf("result was true\n");
+        printf("  result was true\n\n");
     }
     return 0;
 }
