1c7f4302fd9bbf739f676cdb08ddece2faa6cb6d
Nikita Tchayka <nikitatchayka@gmail.com>
2015-02-26 16:47:47 +0000

    Make some functions private

--- EaCTest.h
+++ EaCTest.h
@@ -1,10 +1,10 @@
 #ifndef __EACTEST__
 #define __EACTEST__
 
-void assertIntEquals(char* description, int expected, int actual);
-void assertStringEquals(char* description, char* expected, char* actual);
-void assertTrue(char* description, int condition);
-void assertFalse(char* description, int condition);
+void AssertIntEquals(char* description, int expected, int actual);
+void AssertStringEquals(char* description, char* expected, char* actual);
+void AssertTrue(char* description, int condition);
+void AssertFalse(char* description, int condition);
 
 #include <string.h>
 #include <stdio.h>
@@ -13,67 +13,67 @@ void assertFalse(char* description, int condition);
 #define EACTEST_FG_RED "\x1B[31m"
 #define EACTEST_FG_GREEN "\x1B[32m"
 
-void printInGreen(const char* message){
+static void PrintInGreen(const char* message){
     printf(EACTEST_FG_GREEN "%s\n", message);
     printf(EACTEST_NORMAL_COLOR "");
 }
 
-void printInRed(const char* message){
-    printf(EACTEST_FG_RED"%s\n", message);
+static void PrintInRed(const char* message){
+    printf(EACTEST_FG_RED "%s\n", message);
     printf(EACTEST_NORMAL_COLOR "");
 }
 
-void pass(char* description){
+void Pass(char* description){
     char message[128] = "\0";
     strcat(message, description);
     strcat(message, " [PASSED]\n");
-    printInGreen(message);
+    PrintInGreen(message);
 }
 
-void fail(char* description){
+void Fail(char* description){
     char message[128]  = "\0";
     strcat(message, description);
     strcat(message, " [FAILED]");
-    printInRed(message);
+    PrintInRed(message);
 }
 
-void assertIntEquals(char* description, int expected, int actual){
+void AssertIntEquals(char* description, int expected, int actual){
     if(expected == actual){
-        pass(description);
+        Pass(description);
     }
     else{
-        fail(description);
+        Fail(description);
         printf("  %d is not %d\n\n", expected, actual);
     }
 }
 
-void assertStringEquals(char* description, char* expected, char* actual){
+void AssertStringEquals(char* description, char* expected, char* actual){
     if(strcmp(expected, actual) == 0){
-        pass(description);
+        Pass(description);
     }
     else{
-        fail(description);
+        Fail(description);
         printf("  %s is not %s\n\n", expected, actual);
     }
 }
 
-void assertTrue(char* description, int condition){
+void AssertTrue(char* description, int condition){
     if(condition){
-        pass(description);
+        Pass(description);
     }
     else{
-        fail(description);
+        Fail(description);
         printf("  Expected true but was false\n\n");
     }
 }
 
-void assertFalse(char* description, int condition){
+void AssertFalse(char* description, int condition){
     if(condition){
-        fail(description);
+        Fail(description);
         printf("  Expected false but was true\n\n");
     }
     else{
-        pass(description);
+        Pass(description);
     }
 }
 
